7e610ce5bdedb10a75bf67a33f063262869004e4
Alkdis Chen <2781530336@qq.com>
2023-08-20 12:10:07 +0800

    update DenseNet

---
+++ DenseNet/2023.8.20 pre/clean-messy-rooms-main/clean-messy-rooms-main/test.py
@@ -0,0 +1,146 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import matplotlib.pyplot as plt
+
+# Define data paths
+data_dir = 'C:/Users/15156/Desktop/clean-messy-rooms-main/clean-messy-rooms-main/tmp/images'
+train_dir = os.path.join(data_dir, 'train')
+val_dir = os.path.join(data_dir, 'val')
+test_dir = os.path.join(data_dir, 'test')
+
+# Define data transforms
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+    ]),
+}
+
+# Create datasets
+train_dataset = datasets.ImageFolder(train_dir, data_transforms['train'])
+val_dataset = datasets.ImageFolder(val_dir, data_transforms['val'])
+test_dataset = datasets.ImageFolder(test_dir, data_transforms['test'])
+
+# Create data loaders
+batch_size = 32
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size)
+test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+# Define DenseNet model
+import torchvision.models as models
+
+model = models.densenet121(pretrained=True)
+num_ftrs = model.classifier.in_features
+model.classifier = nn.Linear(num_ftrs, 2)  # 2 classes: messy and clean
+device = torch.device("cpu" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training loop
+num_epochs = 50
+train_acc_history = []
+val_acc_history = []
+train_loss_history = []  # Track training loss
+val_loss_history = []  # Track validation loss
+
+for epoch in range(num_epochs):
+    # Training phase
+    model.train()
+    train_corrects = 0
+    train_loss = 0
+
+    for inputs, labels in train_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        _, preds = torch.max(outputs, 1)
+        train_corrects += torch.sum(preds == labels.data)
+        train_loss += loss.item() * inputs.size(0)  # Accumulate training loss
+
+    train_acc = train_corrects.double() / len(train_dataset)
+    train_loss = train_loss / len(train_dataset)
+    train_acc_history.append(train_acc)
+    train_loss_history.append(train_loss)
+
+    # Validation phase
+    model.eval()
+    val_corrects = 0
+    val_loss = 0
+
+    with torch.no_grad():
+        for inputs, labels in val_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            val_corrects += torch.sum(preds == labels.data)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item() * inputs.size(0)  # Accumulate validation loss
+
+    val_acc = val_corrects.double() / len(val_dataset)
+    val_loss = val_loss / len(val_dataset)
+    val_acc_history.append(val_acc)
+    val_loss_history.append(val_loss)
+
+    print(f'Epoch [{epoch + 1}/{num_epochs}], '
+          f'Train Accuracy: {train_acc:.4f}, '
+          f'Validation Accuracy: {val_acc:.4f}, '
+          f'Train Loss: {train_loss:.4f}, '
+          f'Validation Loss: {val_loss:.4f}')
+
+# Plot accuracy and loss history
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.plot(range(1, num_epochs + 1), train_acc_history, label='Train')
+plt.plot(range(1, num_epochs + 1), val_acc_history, label='Validation')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Accuracy History')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(range(1, num_epochs + 1), train_loss_history, label='Train')
+plt.plot(range(1, num_epochs + 1), val_loss_history, label='Validation')
+plt.xlabel('Epoch')
+plt.ylabel('Loss)')
+plt.title('Loss History')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+# Testing phase
+model.eval()
+test_corrects = 0
+
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        _, preds = torch.max(outputs, 1)
+        test_corrects += torch.sum(preds == labels.data)
+
+test_acc = test_corrects.double() / len(test_dataset)
+print(f'Test Accuracy: {test_acc:.4f}')
